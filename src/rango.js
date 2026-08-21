fa77cfc93083d46d6ee7463eefc38cd87c50b081
Roberto Arce <robertoarcee@gmail.com>
2026-08-21 07:41:00 -0400

    Alguien más tocó la misma línea en main

--- src/rango.js
+++ src/rango.js
@@ -5,7 +5,7 @@
  */
 function sumaRango(start, end) {
   let total = 0;
-  for (let i = start; i < end; i++) {
+  for (let i = start; i < end + 1; i++) { // tocado en main
     total += i;
   }
   return total;
